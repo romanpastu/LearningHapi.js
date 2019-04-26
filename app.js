149a3567c53983cb7bc534a5f1593a702f356595
romanpastu <roman.pastushenko.1997@gmail.com>
2019-04-26 10:16:00 +0200

    block info api implementation

--- app.js
+++ app.js
@@ -5,31 +5,38 @@ const Wreck = require('wreck');
 
 const server = new Hapi.Server({
     "host": "localhost",
-    "port": 3005
+    "port": 3000
 })
 
 Mongoose.connect('mongodb://localhost/mchain', { useNewUrlParser: true })
 
-const BlockModel = Mongoose.model('block',{
+const BlockModel = Mongoose.model('block', {
     height: Number,
     size: Number,
     time: Number
 })
 
 server.route({
-    method:"GET",
-    path:"/",
-    handler: async (request,h) => {
+    method: "GET",
+    path: "/",
+    handler: async (request, h) => {
+        await BlockModel.deleteMany({})
         const { res, payload } = await Wreck.get('https://explorer.mchain.network/api/blocks?limit=50');
         let myJson = JSON.parse(payload.toString()).blocks
-
-         for (let i = 0 ; myJson.length;i++){
-            var block = new BlockModel({height: myJson[i].height, size : myJson[i].size, time: myJson[i].time});
+        console.log(myJson)
+        for (let i = 0; i<myJson.length; i++) {
+            var block = new BlockModel({  height: myJson[i].height, size: myJson[i].size, time: myJson[i].time });
             block.save();
-         }
 
+            delete myJson[i].hash;
+            delete myJson[i].txlength;
+            delete myJson[i].poolInfo;
+            delete myJson[i].isMainChain;
+            delete myJson[i].minedBy;
+            
+        }
 
-        return "hola"
+        return myJson;
     }
 })
 
