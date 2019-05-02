0b235d2921df9c01c51dfd4e0bc2c2f3cbdb6419
romanpastu <roman.pastushenko.1997@gmail.com>
2019-05-02 13:59:52 +0200

    app modularization

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const Hapi = require('hapi')
 const Mongoose = require('mongoose')
-const Wreck = require('wreck');
+const routes = require('./routes')
 
 const server = new Hapi.Server({
     "host": "localhost",
@@ -8,117 +8,7 @@ const server = new Hapi.Server({
 })
 
 Mongoose.connect('mongodb://localhost/mchain', { useNewUrlParser: true })   //indica url de la bd
-//
-const schema = new Mongoose.Schema({
-    hash: String,
-    height: Number,
-    size: Number,
-    time: Number
-  })
 
-const BlockModel = Mongoose.model('blocks', schema)
-
-
-//
-
-
-const StatsModel = Mongoose.model('stats', {      //modelo stats de la bd
-    mined_currency_amount: Number,
-    number_of_transactions: Number,
-    difficulty: Number
-
-})
-//gets and updates blockinfo
-server.route({
-    method: "GET",
-    path: "/api/updateblockinfo",
-    handler: async (request, h) => {
-        await BlockModel.deleteMany({})
-        const { res, payload } = await Wreck.get('https://explorer.mchain.network/api/blocks?limit=50');
-        let myJson = JSON.parse(payload.toString()).blocks
-
-        for (let i = 0; i < myJson.length; i++) {
-            var block = new BlockModel({ hash: myJson[i].hash ,height: myJson[i].height, size: myJson[i].size, time: myJson[i].time });
-            await block.save();
-        }
-
-        return BlockModel.find({});
-    }
-})
-//gets block info from the database
-server.route({
-    method: "GET",
-    path: "/api/blockinfo",
-    handler: async (request, h) => {
-        return BlockModel.find({});
-    }
-})
-
-//gets total stats
-server.route({
-    method: "GET",
-    path: "/api/stats",
-    handler: async (request, h) => {
-        await StatsModel.deleteMany({});
-        const { res, payload } = await Wreck.get('https://explorer.mchain.network/api/statistics/total');
-        let myJson = JSON.parse(payload.toString());
-
-        var stats = new StatsModel({
-            mined_currency_amount: myJson.mined_currency_amount,
-            number_of_transactions: myJson.number_of_transactions,
-            difficulty: myJson.difficulty
-        });
-        await stats.save();
-
-        return StatsModel.find({});
-    }
-})
-
-//get block by hash
-server.route({
-    method: "GET",
-    path:"/api/blockinfo/{hash}",
-    handler: async (request,h) => {
-        try{
-            var block = await BlockModel.findOne({hash: request.params.hash}).exec();
-            return h.response(block);
-        }catch(error){
-            console.log(request.params.hash)
-            return h.response(error).code(500);
-        }   
-    }
-})
-
-//Delete block by block id
-server.route({
-    method: "DELETE",
-    path: "/api/blockinfo/{hash}",
-    handler: async (request,h) => {
-        try{
-            var result = await BlockModel.findOneAndDelete({hash: request.params.hash.toString()});
-            return h.response(result);
-        }catch(error){
-            console.log(request.params.hash)
-            return h.response(error).code(500);
-        }
-    }
-})
-
-//Update block
-
-server.route({
-    method: "PUT",
-    path:"/api/blockinfo/{hash}",
-    handler: async (request, h) => {
-         try{
-            const { hash } = request.params
-            var result = await BlockModel.findOneAndUpdate({ hash },  JSON.parse(request.payload ),  {new: true});
-            return h.response(result);
-        }catch{
-            return h.response(error).code(500);
-        }
-    }
-})
-
+server.route(routes);
 
 server.start();
