78c2861f5cbd4620503c03026798c08fdfe0b254
romanpastu <roman.pastushenko.1997@gmail.com>
2019-05-02 14:49:22 +0200

    cors policity tru

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const routes = require('./routes')
 
 const server = new Hapi.Server({
     "host": "localhost",
-    "port": 3000
+    "port": 3000,
+    routes: { cors: true } 
+    
 })
 
 Mongoose.connect('mongodb://localhost/mchain', { useNewUrlParser: true })   //indica url de la bd
